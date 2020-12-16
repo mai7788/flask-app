fd57ca9bea705d029dd59364799d7585dfa50c8d
whale <whalebox4.5@gmail.com>
2020-12-17 00:19:51 +0800

    change_name

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # return "ok"
+    return render_template('index.html')
+
+@app.route('/article')
+def article():
+    return render_template('generic.html')
+
+@app.route('/element')
+def element():
+    return render_template('elements.html')
+
+app.run()
